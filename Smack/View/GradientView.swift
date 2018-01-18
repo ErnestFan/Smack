3deefe0495e2d96107948089dc68b3b554deef9e
ErnestFan <ernestfan1019@gmail.com>
2018-01-17 20:15:03 -0800

    Add gradient view, add color to layer, add layer to subview

---
+++ Smack/View/GradientView.swift
@@ -0,0 +1,21 @@
+//
+//  GradientView.swift
+//  Smack
+//
+//  Created by Ernest Fan on 2018-01-16.
+//  Copyright © 2018 ERF. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
