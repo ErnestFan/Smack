51d91c32083116319e68a335ae74bbb5953672eb
ErnestFan <ernestfan1019@gmail.com>
2018-01-17 21:03:10 -0800

    preformSegue for login, login screen, dismiss login screen

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Ernest Fan on 2018-01-17.
+//  Copyright © 2018 ERF. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Ernest Fan on 2018-01-17.
+//  Copyright © 2018 ERF. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
