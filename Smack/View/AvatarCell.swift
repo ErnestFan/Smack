930a5498f7802f80697eb563784c30f8e6efd071
ErnestFan <ernestfan1019@gmail.com>
2018-01-23 17:58:37 -0800

    Collection View, AvatarCell, AvatarPickerVC

---
+++ Smack/View/AvatarCell.swift
@@ -0,0 +1,27 @@
+//
+//  AvatarCell.swift
+//  Smack
+//
+//  Created by Ernest Fan on 2018-01-23.
+//  Copyright © 2018 ERF. All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    
+    @IBOutlet weak var avatarImg: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setUpView()
+    }
+    
+    func setUpView() {
+        self.layer.backgroundColor = UIColor.lightGray.cgColor
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+    }
+    
+    
+}
