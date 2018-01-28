59ac6a06e115b69581f30b161e1a58e0ded97005
ErnestFan <ernestfan1019@gmail.com>
2018-01-28 06:21:00 -0800

    Socket Service, emit and on, tableView reload after new channel is added

---
+++ Smack/Services/SocketService.swift
@@ -0,0 +1,47 @@
+//
+//  SocketService.swift
+//  Smack
+//
+//  Created by Ernest Fan on 2018-01-28.
+//  Copyright © 2018 ERF. All rights reserved.
+//
+
+import UIKit
+import SocketIO
+
+class SocketService: NSObject {
+
+    static let instance = SocketService()
+    
+    override init() {
+        super.init()
+    }
+    
+    var socket : SocketIOClient = SocketIOClient(socketURL: URL(string: BASE_URL)!)
+    
+    func establishConnection() {
+        socket.connect()
+    }
+    
+    func closeConnection() {
+        socket.disconnect()
+    }
+    
+    func addChanel(channelName: String, channelDescription: String, completion: @escaping CompletionHandler) {
+        socket.emit("newChannel", channelName, channelDescription)
+        completion(true)
+    }
+    
+    func getChannel(completion: @escaping CompletionHandler) {
+        socket.on("channelCreated") { (dataArray, ack) in
+            guard let channelName = dataArray[0] as? String else { return }
+            guard let channelDescription = dataArray[1] as? String else { return }
+            guard let channelId = dataArray[2] as? String else { return }
+            
+            let newChannel = Channel(_id: channelId, name: channelName, description: channelDescription, __v: 0)
+            
+            MessageService.instance.channels.append(newChannel)
+            completion(true)
+        }
+    }
+}
