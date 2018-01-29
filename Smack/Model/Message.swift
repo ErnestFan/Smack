d3a7c0c843884a484f1debde0aeb45b6c4c8844d
ErnestFan <ernestfan1019@gmail.com>
2018-01-28 19:52:40 -0800

    Message Model, get message request

---
+++ Smack/Model/Message.swift
@@ -0,0 +1,21 @@
+//
+//  Message.swift
+//  Smack
+//
+//  Created by Ernest Fan on 2018-01-28.
+//  Copyright © 2018 ERF. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    public private(set) var message: String!
+    public private(set) var userName: String!
+    public private(set) var channelId: String!
+    public private(set) var userAvatar: String!
+    public private(set) var userAvatarColor: String!
+    public private(set) var id: String!
+    public private(set) var timeStamp: String!
+    
+    
+}
